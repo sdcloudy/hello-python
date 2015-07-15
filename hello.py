e60c51c97c15296adf91cbac540dd493cc2ee9ba
sdcloudy <d.hanacek@gmx.net>
2015-07-15 16:48:49 +0200

    updated code with steteful counter

--- hello.py
+++ hello.py
@@ -5,23 +5,27 @@ app = Flask(__name__)
 my_uuid = str(uuid.uuid1())
 BLUE = "#0099FF"
 GREEN = "#33CC33"
-
+counter = 0
 COLOR = BLUE
 
 @app.route('/')
 def hello():
-
+        global counter
+        counter = counter + 1
 	return """
 	<html>
 	<body bgcolor="{}">
 
-	<center><h1><font color="white">Hi, I'm GUID:<br/>
+	<center><h1><font color="white">Hi, I'm GUID:
 	{}
+	</h1><br/>
+	<h1><font color="white">Hits: 
+	{}
+	</h1><br/>
 	</center>
 
 	</body>
 	</html>
-	""".format(COLOR,my_uuid)
-
+	""".format(COLOR,my_uuid,counter)
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('VCAP_APP_PORT', '5000')))
